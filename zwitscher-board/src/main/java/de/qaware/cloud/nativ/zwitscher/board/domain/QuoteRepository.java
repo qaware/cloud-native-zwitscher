cd43060ee769f4abcb1941ce8ba8958565eb6e3b
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-03-11 01:31:02 +0100

    Implemented basic quotes board. Tweets are still missing.

---
+++ zwitscher-board/src/main/java/de/qaware/cloud/nativ/zwitscher/board/domain/QuoteRepository.java
@@ -0,0 +1,44 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 QAware GmbH, Munich, Germany
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.qaware.cloud.nativ.zwitscher.board.domain;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * The feign client to access the /quote service from our zwitscher-service. Feign
+ * will locate the endpoint via Eureka using the serviceId and use Ribbon to balance
+ * between the registered instances.
+ */
+@FeignClient(value = "zwitscher-service", fallback = Quote.Fallback.class)
+public interface QuoteRepository {
+    /**
+     * Get the next quote from the the /quote service.
+     *
+     * @return the quote
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/quote")
+    Quote getNextQuote();
+}
