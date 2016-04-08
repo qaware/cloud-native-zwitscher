1c68c21d107ee1514187b302098f6e0e02154568
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-04-08 14:43:13 +0200

    Fixed compile error after dependency update.

--- zwitscher-board/src/main/java/de/qaware/cloud/nativ/zwitscher/board/ZwitscherBoardApplication.java
+++ zwitscher-board/src/main/java/de/qaware/cloud/nativ/zwitscher/board/ZwitscherBoardApplication.java
@@ -28,9 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
 import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
@@ -52,8 +50,8 @@ public class ZwitscherBoardApplication {
 
     @Bean
     @LoadBalanced
-    public RestTemplate zwitscherServiceRestTemplate(SpringClientFactory clientFactory, LoadBalancerClient loadBalancer) {
-        RibbonClientHttpRequestFactory requestFactory = new RibbonClientHttpRequestFactory(clientFactory, loadBalancer);
+    public RestTemplate zwitscherServiceRestTemplate(SpringClientFactory clientFactory) {
+        RibbonClientHttpRequestFactory requestFactory = new RibbonClientHttpRequestFactory(clientFactory);
         return new RestTemplate(requestFactory);
     }
 
