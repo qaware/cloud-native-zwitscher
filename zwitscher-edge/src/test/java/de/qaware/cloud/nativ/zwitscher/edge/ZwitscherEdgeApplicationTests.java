7e8c65a64063e14d9dada35df81c43b13bbcc713
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-04-20 00:14:31 +0200

    Added failFast feature to bootstrap. Closes #12.

--- zwitscher-edge/src/test/java/de/qaware/cloud/nativ/zwitscher/edge/ZwitscherEdgeApplicationTests.java
+++ zwitscher-edge/src/test/java/de/qaware/cloud/nativ/zwitscher/edge/ZwitscherEdgeApplicationTests.java
@@ -26,12 +26,14 @@ package de.qaware.cloud.nativ.zwitscher.edge;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ZwitscherEdgeApplication.class)
 @WebAppConfiguration
+@TestPropertySource("classpath:/application-test.properties")
 public class ZwitscherEdgeApplicationTests {
 
     @Test
