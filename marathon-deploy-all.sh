c85d209766e91be86793ddbf69752997dab0b756
Andreas Zitzelsberger <az@az82.de>
2016-07-19 18:15:44 +0200

    Dynamische Ports... Leider registriert sich eureka hartnäckig mit der Docker-IP...

--- marathon-deploy-all.sh
+++ marathon-deploy-all.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 MARATHON_API="http://m1.dcos:8080/v2"
-BASE_PATH="$(dirname "$0")"
+BASE_PATH="$(dirname "$(readlink -f "$0")")"
+DELAY=30
 
 shopt -s globstar
 
 for f in "$BASE_PATH"/*/marathon-*.json
 do
     echo "Deploying $f"
+    # Deploy using the Marathon REST API
     curl -X POST "$MARATHON_API/apps" -H "Content-type: application/json" -d "@$f"
     echo
-    sleep 5
+    # Wait to account for image download times
+    sleep $DELAY
 done
